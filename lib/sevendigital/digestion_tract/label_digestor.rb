c40dda5af4cbda1612dab474161db852caa180d2
filip <filip@7digital.com>
2010-09-02 16:04:22 +0100

    got rid off debugging puts

--- lib/sevendigital/digestion_tract/label_digestor.rb
+++ lib/sevendigital/digestion_tract/label_digestor.rb
@@ -6,13 +6,11 @@ module Sevendigital
     def default_list_element_name; :labels end
     
     def from_proxy(label_proxy)
-      puts "eating label"
       make_sure_not_eating_nil(label_proxy)
 
       label = Label.new()
       label.id = label_proxy.id.to_i
       label.name = label_proxy.name.value.to_s
-      puts label_proxy.to_s
 
       return label
       end
