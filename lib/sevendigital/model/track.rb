b36ab6eb0424421c65120967ffb9260d534a4004
unknown <filip@.(none)>
2010-09-08 22:19:43 +0100

    track.get_details track.short_title track.similar?

--- lib/sevendigital/model/track.rb
+++ lib/sevendigital/model/track.rb
@@ -1,8 +1,24 @@
 module Sevendigital 
 
   class Track < SevendigitalObject
-    attr_accessor :id, :title, :version, :artist, :track_number,
-                  :duration, :explicit_content, :isrc, :release, :url, :price
+    attr_accessor :id, :title, :version, :artist
+                  
+    sevendigital_basic_property :track_number,:duration, :explicit_content, :isrc, :release, :url, :price
+
+    def get_details(options={})
+      track_with_details = @api_client.track.get_details(@id, options)
+      copy_basic_properties_from(track_with_details)
+    end
+    
+    def short_title
+  #   return title.gsub(/\s+[\(\[](album|lp|single|short|edit|radio)\s+version[\)\]]/, "")
+      return title.gsub(/\s+\(.*\s(version|mix|remix|edit)\s*\)/, "")
+    end
+
+    def similar?(the_other_track)
+      return the_other_track && short_title == the_other_track.short_title \
+            && the_other_track.artist && artist.name == the_other_track.artist.name
+    end
 
   end
 end
---
+++ spec/model/track_spec.rb
@@ -0,0 +1,111 @@
+require "spec"
+require "date"
+require 'sevendigital'
+
+describe "Track" do
+
+  before do
+    @client = stub(Sevendigital::Client)
+    @track_manager = mock(Sevendigital::TrackManager)
+    @client.stub!(:track).and_return @track_manager
+    
+    @track = Sevendigital::Track.new(@client)
+    @track.id = 1234
+  end
+
+  it "get_details should get track's basic details from manager" do
+    expected_options = {:page => 2}
+    fresh_track = fake_track_with_details
+
+    @track_manager.should_receive(:get_details) { |track_id, options|
+      track_id.should == @track.id
+      (options.keys & expected_options.keys).should == expected_options.keys
+      fresh_track
+    }
+    @track.get_details(expected_options)
+    
+    @track.track_number.should == fresh_track.track_number
+    @track.duration.should == fresh_track.duration
+    @track.explicit_content.should == fresh_track.explicit_content
+    @track.isrc.should == fresh_track.isrc
+    @track.release.should == fresh_track.release
+    @track.url.should == fresh_track.url
+
+  end
+
+  it "should have a short title without any version in brackets" do
+
+    @track.title = "track 1 (lp version)"
+    @track.short_title.should == "track 1"
+    @track.title = "(lp version)"
+    @track.short_title.should == "(lp version)"
+    @track.title = "track 1 (something else)"
+    @track.short_title.should == "track 1 (something else)"
+    @track.title = "track 1 (dj remix)"
+    @track.short_title.should == "track 1"
+    @track.title = "track  1  (  dj mix  )"
+    @track.short_title.should == "track  1"
+  end
+
+  describe "similar?" do
+
+    before do
+      @artist1 = Sevendigital::Artist.new(@client)
+      @artist2 = Sevendigital::Artist.new(@client)
+      @track1 = Sevendigital::Track.new(@client)
+      @track1.artist = @artist1
+      @track2 = Sevendigital::Track.new(@client)
+      @track2.artist = @artist2
+
+      @artist1.name = "artist 1"
+      @track1.title = "track 1"
+
+    end
+
+    it "should be similar to another track if artist names and titles match"  do
+      @artist2.name = "artist 1"
+      @track2.title = "track 1"
+
+      @track1.similar?(@track2).should == true
+    end
+
+    it "should be similar to another track if artist names and short titles match"  do
+      @track1.title = "track 1 (uk version)"
+     
+      @artist2.name = "artist 1"
+      @track2.title = "track 1 (us remix)"
+
+      @track1.similar?(@track2).should == true
+    end
+
+    it "should not be similar to another track if artist names don't match" do
+      @artist2.name = "artist 2"
+      @track2.title = "track 1"
+
+      @track1.similar?(@track2).should == false
+    end
+
+    it "should not be similar to another track if short titles match" do
+      @artist2.name = "artist 1"
+      @track2.title = "track 2"
+
+      @track1.similar?(@track2).should == false
+    end
+
+
+  end
+
+  def fake_track_with_details
+    track = Sevendigital::Track.new(@client)
+    track.title = "Song 1"
+    track.version = "Version 2"
+    track.artist = Sevendigital::Artist.new(@client)
+    track.track_number = 7
+    track.duration = 77
+    track.explicit_content = true
+    track.isrc = "7D"
+    track.release = Sevendigital::Release.new(@client)
+    track.url = "http://aaa.bbb.ccc/"
+    track
+  end
+end
