06ffa96190315bc311b550febf17b8274331f01d
filip <filip@7digital.com>
2010-10-15 17:51:27 +0100

    don't cache signed api requests

--- lib/sevendigital/api_operator_cached.rb
+++ lib/sevendigital/api_operator_cached.rb
@@ -9,11 +9,11 @@ class ApiOperatorCached < ApiOperator
 
   def call_api(api_request)
     uri = create_request_uri(api_request)
-    api_response = @cache.get(uri.to_s)
+    api_response = @cache.get(uri.to_s) if !api_request.requires_signature?
     puts "ApiOperatorCached: Got from cache #{uri}" if api_response if @client.verbose?
     if (!api_response) then
       api_response = make_http_request_and_digest(api_request)
-      @cache.set(uri.to_s, api_response)
+      @cache.set(uri.to_s, api_response) if !api_request.requires_signature?
     end
     puts api_response if @client.very_verbose?    
     api_response
--- spec/api_operator_cached_spec.rb
+++ spec/api_operator_cached_spec.rb
@@ -9,6 +9,16 @@ describe "ApiOperatorCached" do
     @client.stub!(:very_verbose?).and_return(false)
     @cache = stub(Hash)
     @cached_operator = Sevendigital::ApiOperatorCached.new(@client, @cache)
+    @stub_api_request = stub_api_request()
+  end
+
+  it "should not look into cache if request requires signature" do
+    api_response = fake_api_response()
+    @stub_api_request.stub!(:requires_signature?).and_return(true)
+    @cached_operator.stub(:create_request_uri).and_return("key")
+    @cache.stub!(:get).with("key").and_return(nil)
+    @cached_operator.should_receive(:make_http_request_and_digest).with(@stub_api_request).and_return(api_response)
+    @cached_operator.call_api(@stub_api_request)
   end
 
   it "should not make an http request if response already in cache " do
@@ -24,13 +34,13 @@ describe "ApiOperatorCached" do
     @cached_operator.stub(:create_request_uri).and_return("key")
     @cache.stub!(:get).with("key").and_return(nil)
     @cache.stub!(:set).and_return(nil)
-    @cached_operator.should_receive(:make_http_request_and_digest).with("key").and_return(api_response)
+    @cached_operator.should_receive(:make_http_request_and_digest).with(@stub_api_request).and_return(api_response)
     
     response = @cached_operator.call_api(@stub_api_request)
     response.should == api_response
   end
 
-  it "should cache uncached api response" do
+  it "should cache uncached api response if request was not signed" do
     api_response = fake_api_response()
 
     @cached_operator.stub(:create_request_uri).and_return("key")
@@ -41,8 +51,32 @@ describe "ApiOperatorCached" do
     response = @cached_operator.call_api(@stub_api_request)
   end
 
+  it "should not cache api response if request was signed" do
+    api_response = fake_api_response()
+    @stub_api_request.stub!(:requires_signature?).and_return(true)
+
+    @cached_operator.stub(:create_request_uri).and_return("key")
+    @cache.stub!(:get).with("key").and_return(nil)
+    @cached_operator.stub!(:make_http_request_and_digest).and_return(api_response)
+
+    @cache.should_not_receive(:set)
+    response = @cached_operator.call_api(@stub_api_request)
+  end
+
   def fake_api_response
     return Net::HTTP.new("1.1", 200, "response_body")
   end
+
+  def stub_api_request
+  api_request = stub(Sevendigital::ApiRequest)
+
+  api_request.stub!(:parameters).and_return({})
+  api_request.stub!(:api_service).and_return(nil)
+  api_request.stub!(:api_method).and_return("m")
+  api_request.stub!(:requires_signature?).and_return(false)
+  api_request.stub!(:requires_secure_connection?).and_return(false)
+  api_request.stub!(:ensure_country_is_set)
+  return api_request
+end
 
 end
