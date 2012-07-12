81fc8844d00375bd96dbc06e11ab9a2b20671918
filip <f@d.com>
2012-07-12 21:59:25 +0100

    added track/search/beta

--- lib/sevendigital/management/track_manager.rb
+++ lib/sevendigital/management/track_manager.rb
@@ -23,5 +23,11 @@ module Sevendigital
       api_response = @api_client.make_api_request(:GET, "track/search", {:q => query}, options)
       @api_client.track_digestor.nested_list_from_xml_doc(api_response.item_xml("searchResults"), :searchResult, :track)
     end
+
+    def search_beta(query, options={})
+      api_response = @api_client.make_api_request(:GET, "track/search/beta", {:q => query}, options)
+      @api_client.track_digestor.nested_list_from_xml_doc(api_response.item_xml("searchResults"), :searchResult, :track)
+    end
+
   end
 end
